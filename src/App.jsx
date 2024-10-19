e633b2465203101b80cf5ed04a24682476cc4b7f
Mohammed Shamil <kmshamil123@gmail.com>
2024-10-19 13:46:11 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,23 @@
-import './App.css'
-import { useState } from 'react'
 import { BrowserRouter } from 'react-router-dom';
+
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Features from './components/Features.jsx';
 import Testimonial from './components/Testimonial';
-
-
+import CallToAction from './components/CallToAction';
+import Footer from './components/Footer';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <BrowserRouter>
       <Navbar />
       <Hero />
       <Features />
       <Testimonial />
+      <CallToAction />
+      <Footer />
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
